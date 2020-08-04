87e20b6bc7826878d2afe72fd0bc61dd6a4403ae
Scott Franz <IceColdCoder@users.noreply.github.com>
2020-08-04 02:13:26 -0600

    Implementing HTML parser tests.

---
+++ mzml_web_scraper/HTML_Parser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Debug.
+using System.Diagnostics;
+
+namespace mzml_web_scraper
+{
+    public static class HTML_Parser
+    {
+
+        static readonly string specfile = "html_spec.txt";
+        static HTML_Parser(){
+            Read_HTML_Spec(specfile);
+        }
+
+        private static void Read_HTML_Spec(string specfile)
+        {
+            foreach (string line in File.ReadAllLines(specfile))
+            {
+                if (!line.Equals(""))
+                {
+                    string[] line_parts = line.Split(' ');
+                    Debug.WriteLine(line_parts[0]);
+                }
+            }
+        }
+
+        public static void Parse_HTML(string html)
+        {
+
+        }
+    }
+
+}
---
+++ nunit_tests/mzml_web_scraper_nunit_tests/HTML_Parser_Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using mzml_web_scraper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework.Internal;
+
+
+namespace mzml_web_scraper_nunit_tests
+{
+
+    public class HTML_Parser_Test
+    {
+        [SetUp]
+        public void setup()
+        {
+
+        }
+
+        [TearDown]
+        public void dispose()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+
+}
