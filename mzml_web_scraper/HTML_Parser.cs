7d67793fa41f9a15c693d610c2eb5056f658b892
Scott Franz <IceColdCoder@users.noreply.github.com>
2020-09-23 18:25:08 -0600

    Added more string parsing functionality.

--- mzml_web_scraper/HTML_Parser.cs
+++ mzml_web_scraper/HTML_Parser.cs
@@ -30,74 +30,43 @@ namespace mzml_web_scraper
             using (StreamReader file = new StreamReader(specfile))
             {
                 string line = "";
-                Queue<char> open_q = new Queue<char>();
-                Queue<char> close_q = new Queue<char>();
+
                 string html_pattern = @"<.*?\s??.*?>";
                 Regex html_regex = new Regex(html_pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+                string tag_pattern = @"<.*?>";
+                Regex tag_regex = new Regex(tag_pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
                 while ((line = file.ReadLine()) != null)
                 {
 
-                    if(line.Length > 1)
+                    if (line.Length > 1)
                     {
                         Match match = html_regex.Match(line);
-                        Debug.WriteLine(match.Value + " at " + match.Index + " with length " + match.Length);
+
+                        //Case for <...> </...> IE normal Tags.
+                        if (tag_regex.IsMatch(match.Value))
+                        {
+                            string html_opener = match.Value;
+                            string html_closer = match.Value.Insert(1, "/");
+
+                            html_spec.Add(html_opener, html_closer);
+                            html_spec.Add(html_closer, html_opener);
+                        }
+
+                        //Case for <! ... >
+                        else
+                        {
+
+                        }
+
+
+
                     }
 
-
-                    //if (!line.Equals(""))
-                    //{
-                    //    //General case.
-                    //    if (line[1] != '!')
-                    //    {
-                    //        int i = 1;
-                    //        StringBuilder open_statement = new StringBuilder();
-                    //        StringBuilder close_statement = new StringBuilder();
-                    //        open_statement.Append("<");
-                    //        close_statement.Append("</");
-           
-
-                    //        while (line[i] != '>' && i < line.Length)
-                    //        {
-                    //            open_statement.Append(line[i]);
-                    //            close_statement.Append(line[i]);
-                    //            i++;
-                    //        }
-
-                    //        open_statement.Append(">");
-                    //        close_statement.Append(">");
-
-                    //        string open_string = open_statement.ToString();
-                    //        string close_string = close_statement.ToString();
-
-                    //        html_spec.Add(open_string, close_string);
-                    //        html_spec.Add(close_string, open_string);
-                    //    }
-
-                    //    //Edge cases.
-                    //    else
-                    //    {
-                    //        int i = 1;
-                    //        StringBuilder open_statement = new StringBuilder();
-                    //        StringBuilder close_statement = new StringBuilder();
-                    //        open_statement.Append("<");
-                    //        close_statement.Append("</");
-                   
-                    //        //For <!Doctype html>
-                    //        if(Char.IsLetterOrDigit(line[i + 1]))
-                    //        {
-                    //            StringBuilder open_statement = new StringBuilder();
-                    //            open_statement.Append("<!");
-                    //        }
-                    //        //For <!-- -->
-                    //        else
-                    //        {
-
-                    //        }
-
-                    //    }
-                    //}
-
                 }
+
+
             }
 
             foreach(DictionaryEntry tmp in html_spec)
